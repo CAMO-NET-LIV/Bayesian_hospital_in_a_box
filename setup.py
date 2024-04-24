8217aa46b0d1b3ec0429e00285a0b3b5211451db
Alessandro Gerada <62601884+agerada@users.noreply.github.com>
2024-04-24 15:28:44 +0100

    implemented des draft

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 #packages = find_packages(".", exclude=["tests"])
 packages = find_packages(".")
 setup(
-    name='hospital_in_a_box',
+    name='bayesian_hospital_in_a_box',
     version='0.1.0',
     author='Peter L Green, Alessandro Gerada, Conor Rosato',
     author_email='p.l.green@liverpool.ac.uk',
     packages=packages,
-    install_requires=["numpy", "scipy"]
+    install_requires=["numpy", "scipy", "salabim"]
 )
 
