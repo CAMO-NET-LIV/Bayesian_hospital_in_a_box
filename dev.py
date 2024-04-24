dcf44be3345daae391ade64139459f0d46961280
plgreenLIRU <p.l.green@liverpool.ac.uk>
2024-04-24 16:26:45 +0100

    temp commit of dev script

---
+++ dev.py
@@ -0,0 +1,6 @@
+import bayesian_hospital_in_a_box
+from tests.test_likelihood import test_monte_carlo_estimate, test_p, test_first_term
+
+#test_first_term(plots=True)
+#test_monte_carlo_estimate(plots=True)
+test_p(plots=True)
