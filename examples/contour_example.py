861d6054bcbbc167d4258c81dc2705cc0ea409ec
Pete Green <p.l.green@liverpool.ac.uk>
2024-05-07 14:12:10 +0100

    contour example working

--- examples/contour_example.py
+++ examples/contour_example.py
@@ -9,11 +9,11 @@ df = pd.read_csv('results.csv')
 t = df['time_completed'].values
 
 # No. of simulation samples we will include
-N_samples = 5
+N_samples = 50
 
 # Range of averge times over which we'll create contour plot
-r_hrs = np.linspace(1, 10, 20)
-l_hrs = np.linspace(1, 10, 20)
+r_hrs = np.linspace(1, 10, 40)
+l_hrs = np.linspace(1, 10, 40)
 
 # Initialise contour
 R_hrs = np.zeros([len(r_hrs), len(l_hrs)])
@@ -21,15 +21,18 @@ L_hrs = np.zeros([len(r_hrs), len(l_hrs)])
 P = np.ones([len(r_hrs), len(l_hrs)])
 
 # Create contour with likelihood evaluations
+n = 0
+N_total = len(r_hrs) * len(l_hrs)
 for i in range(len(r_hrs)):
     for j in range(len(l_hrs)):
         R_hrs[i, j] = r_hrs[i]
         L_hrs[i, j] = l_hrs[j]
         theta = [1 / (60 * r_hrs[i]),
                  1 / (60 * l_hrs[j])]
-        #for n in range(N_samples):
-        #    P[i, j] *= p(theta, t[n], pc0=0.2, N_l_max=3)
         P[i, j] = p(theta, t[:N_samples], pc0=0.2, N_l_max=3)
+
+        n += 1
+        print(n / N_total * 100, "percent done")
 
 # Plot
 plt.contourf(R_hrs, L_hrs, P)
