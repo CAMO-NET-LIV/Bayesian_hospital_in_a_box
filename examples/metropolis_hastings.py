4e485c93693e13cff2d66ecb25532ed840f2fbab
plgreenLIRU <p.l.green@liverpool.ac.uk>
2024-05-22 12:05:51 +0100

    code working now

--- examples/metropolis_hastings.py
+++ examples/metropolis_hastings.py
@@ -16,7 +16,7 @@ t = df['time_completed'].values
 t = t[:50]
 
 # Set burn-in
-burn_in = 50
+burn_in = 5000
 
 # Limits of uniform prior (which correspond to average times
 # of between 1 and 10 hours)
@@ -27,7 +27,7 @@ prior_upper = 1 / 60
 theta0 = np.array([0.002, 0.002])
 
 # MCMC
-N = 200
+N = 20000
 theta = np.zeros([N, 2])
 theta[0] = theta0
 logp_now = logp(theta0, t, pc0=0.2, N_l_max=3)
@@ -37,7 +37,7 @@ for i in range(N-1):
     theta_dash = theta[i, :] + 1e-3 * np.random.randn(2)
     
     # Prior limits
-    if np.sum(theta_dash < 0) > 0 or np.sum(theta_dash > prior_upper) > 0:
+    if np.sum(theta_dash < prior_lower) > 0 or np.sum(theta_dash > prior_upper) > 0:
         theta[i+1, :] = theta[i, :]
         
     # Accept / Reject step
@@ -66,7 +66,12 @@ ax[1][0].plot(np.repeat(2, N), np.arange(0, N), 'green')
 ax[1][1].plot(theta_hrs[:, 1], np.arange(0, N), 'black')
 ax[1][1].plot(theta_hrs[:burn_in, 1], np.arange(0, burn_in), 'red', label='Burn in')
 ax[1][1].plot(np.repeat(8, N), np.arange(0, N), 'green', label='True values')
+ax[1][1].legend()
 ax[1][0].set_xlabel('Transport mean (hours)')
 ax[1][1].set_xlabel('Lab mean (hours)')
+ax[0][0].grid()
+ax[0][1].grid()
+ax[1][0].grid()
+ax[1][1].grid()
 plt.tight_layout()
 plt.show()
