7149af8bf8556b635ded42a5f816995656cb3ede
agerada <sandrogerada@gmail.com>
2024-06-18 20:05:20 +0100

    added DES, untested

--- bayesian_hospital_in_a_box/case_studies.py
+++ bayesian_hospital_in_a_box/case_studies.py
@@ -1,5 +1,24 @@
 import numpy as np
 from scipy.stats import expon
+import salabim as sim
+
+
+class SpecimenCaseStudy1(sim.Component):
+    def __init(self, beta_l_true, beta_r_true, log, **kwargs):
+        super().__init__(**kwargs)
+        self.beta_l_true = beta_l_true
+        self.beta_r_true = beta_r_true
+        self.log = log
+        self.log[self.name()] = {}
+
+    def process(self):
+        collection_t = self.env.now()
+        self.log[self.name()]['time_collected'] = collection_t
+        yield self.hold(sim.Exponential(rate=1/self.beta_r_true).sample())
+        self.log[self.name()]['time_received'] = self.env.now() - collection_t
+        yield self.hold(sim.Exponential(rate=1/self.beta_l_true).sample())
+        self.log[self.name()]['time_lab'] = self.env.now() - collection_t
+
 
 class case_study1():
 
@@ -16,7 +35,23 @@ class case_study1():
         self.tl_samples = pl.rvs(N)         # Samples of lab time
         self.t_samples = self.tr_samples + self.tl_samples  # Samples of total time
         self.N = N
-    
+
+    def generate_des_data(self, N):
+        log = {}
+        env = sim.Environment(trace=False)
+        sim.ComponentGenerator(SpecimenCaseStudy1,
+                               beta_l_true=self.beta_l_true,
+                               beta_r_true=self.beta_r_true,
+                               iat=10,
+                               number=N,
+                               log=log)
+        env.run()
+        self.tr_samples = np.array([log[specimen.name()]['time_received'] for specimen in log])
+        self.tl_samples = np.array([log[specimen.name()]['time_lab'] - log[specimen.name()]['time_received']
+                                    for specimen in log])
+        self.t_samples = np.array([log[specimen.name()]['time_lab'] for specimen in log])
+        self.N = N
+
     def __joint_model_log_prior(self, beta_l, beta_r, t_l):
         """
         Log prior for the joint model
