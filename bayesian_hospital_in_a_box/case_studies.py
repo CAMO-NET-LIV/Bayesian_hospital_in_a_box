302b99cea19db97c95155997cc881bc189ead760
Pete Green <p.l.green@liverpool.ac.uk>
2024-06-19 14:14:48 +0100

    First commit of sample_marginal_posteror pymc method

--- bayesian_hospital_in_a_box/case_studies.py
+++ bayesian_hospital_in_a_box/case_studies.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.stats import expon
 import salabim as sim
-
+import pymc as pm
 
 class SpecimenCaseStudy1(sim.Component):
     def __init(self, beta_l_true, beta_r_true, log, **kwargs):
@@ -25,8 +25,8 @@ class case_study1():
     def __init__(self, beta_l_true, beta_r_true):
         self.beta_l_true = beta_l_true
         self.beta_r_true = beta_r_true
-        self.beta_l_prior = expon(scale=10)
-        self.beta_r_prior = expon(scale=10)
+        self.beta_l_prior_scale = 10
+        self.beta_r_prior_scale = 10
     
     def generate_data(self, N):
         pr = expon(scale=self.beta_r_true)  # Distribution over transport time
@@ -52,69 +52,24 @@ class case_study1():
         self.t_samples = np.array([log[specimen.name()]['time_lab'] for specimen in log])
         self.N = N
 
-    def __joint_model_log_prior(self, beta_l, beta_r, t_l):
-        """
-        Log prior for the joint model
-        """
-
-        # Log-prior over beta_l and beta_r
-        ans = self.beta_l_prior.logpdf(beta_l)
-        ans += self.beta_r_prior.logpdf(beta_r)
-        
-        # Log-prior over lab times
-        pt = expon(scale=beta_l)
-        ans += np.sum(pt.logpdf(t_l))
-
-        return ans
-    
-    def __joint_model_log_likelihood(self, beta_l, beta_r, t_l):
-        """
-        Log-likelihood for the joint model
-        """
-
-        # Log-likelihood
-        p = expon(scale=beta_r)
-        ans = np.sum(p.logpdf(self.t_samples - t_l))
-
-        return ans
-    
-    def joint_model_log_posterior(self, theta):
-
-        # Extract parameters
-        beta_l, beta_r, t_l = theta[0], theta[1], theta[2:]
-        
-        # Log posterior
-        ans = self.__joint_model_log_prior(beta_l, beta_r, t_l)
-        ans += self.__joint_model_log_likelihood(beta_l, beta_r, t_l)
-
-        return ans
-    
-    def __marginal_model_log_prior(self, beta_l, beta_r):
-        """
-        Log prior for the marginal model
-        """
-        ans = self.beta_l_prior.logpdf(beta_l)
-        ans += self.beta_r_prior.logpdf(beta_r)
-        return ans
-
-    def __marginal_model_log_likelihood(self, beta_l, beta_r):
-        """
-        Log-likelhood for the marginal model (here we compute
-        it before taking the logarithm at the end)
-        """
+    def __marginal_model_log_likelihood(value, beta_l, beta_r):
+        J = 1
         lambda_l = 1 / beta_l
         lambda_r = 1 / beta_r
+        for i in range(N):
+            J = J * lambda_r * lambda_l / (lambda_l - lambda_r) * (pm.math.exp(-lambda_r * value[i]) - pm.math.exp(-lambda_l * value[i]))
 
-        ans = 1
-        for i in range(self.N):
-            ans *= lambda_r * lambda_l / (lambda_l - lambda_r)
-            ans *= np.exp(-lambda_r * self.t_samples[i])
-            ans *= (1 - np.exp(-(lambda_l - lambda_r) * self.t_samples[i]))
-
-        return np.log(ans)
-
-    def marginal_log_posterior(self, theta):
-        beta_l, beta_r = theta[0], theta[1]
-        ans = self.__marginal_model_log_prior(beta_l, beta_r)
-        ans += self.__marginal_model_log_likelihood(beta_l, beta_r)
+        ans = pm.math.log(J)
         return ans
+
+    def sample_marginal_posterior(self, Ns):
+
+        model = pm.Model()
+        with model:
+            beta_l = pm.Exponential('beta_l', scale=self.beta_l_prior_scale, transform=None)
+            beta_r = pm.Exponential('beta_r', scale=self.beta_l_prior_scale, transform=None)
+            pm.CustomDist('marginal_model_log_likelihood', beta_l, beta_r, logp=self.__marginal_model_log_likelihood, observed=self.t_samples)
+            step = pm.Metropolis()
+            initvals = {'beta_l' : 10, 'beta_r' : 1}
+            trace = pm.sample(Ns, step=step, initvals=initvals, return_inferencedata=False)
+        return trace
